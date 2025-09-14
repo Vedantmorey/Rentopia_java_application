6eba07b94d15a8f07b9210cd1f4121b1fa59f245
Vedant vinod morey <139696537+Vedantmorey@users.noreply.github.com>
2025-09-15 03:14:14 +0530

    Update ChatMessage.java

--- Rentopia_Project/Rentopia/src/main/java/com/Rentopia/Rentopia/Entity/ChatMessage.java
+++ Rentopia_Project/Rentopia/src/main/java/com/Rentopia/Rentopia/Entity/ChatMessage.java
@@ -4,9 +4,9 @@ public class ChatMessage {
     private String content;
     private String sender;
     private String type;
-    private Long propertyId; // <-- ADD THIS FIELD
+    private Long propertyId; 
 
-    // --- Getters and Setters ---
+
     public String getContent() { return content; }
     public void setContent(String content) { this.content = content; }
     public String getSender() { return sender; }
@@ -14,7 +14,7 @@ public class ChatMessage {
     public String getType() { return type; }
     public void setType(String type) { this.type = type; }
 
-    // ADD GETTER AND SETTER FOR propertyId
     public Long getPropertyId() { return propertyId; }
     public void setPropertyId(Long propertyId) { this.propertyId = propertyId; }
+
 }
